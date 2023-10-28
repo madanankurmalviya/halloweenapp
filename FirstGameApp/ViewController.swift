b1fd224df32599ad1834a911d4d294178311aaf5
Ankur Malviya <madanankurmalviya@gmail.com>
2023-10-28 04:45:11 -0700

    modified randomised method

--- FirstGameApp/ViewController.swift
+++ FirstGameApp/ViewController.swift
@@ -9,24 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var pumpkinImageFront: UIImageView!
-    
+
+    @IBOutlet weak var abcf: UIImageView!
     @IBOutlet weak var pumpkinImageBottom: UIImageView!
-    var num = 0
+    //var num = 0
     let pumpkinImage = ["Pumpkin left","Pumpkin Right","Pumpkin Bottom","Pumpkin"]
-    let pumpkinBottom = ["Pumpkin Right","Pumpkin left","Pumpkin","Pumpkin Bottom"]
     override func viewDidLoad() {
 
     }
 
     @IBAction func rollButton(_ sender: UIButton) {
-        if num < pumpkinImage.count {
+    
+        //If logic
+        /**if num < pumpkinImage.count {
             print("The button is clicked.../(num): ",num);
             pumpkinImageFront.image = UIImage(imageLiteralResourceName: pumpkinImage[num])
             pumpkinImageBottom.image = UIImage(imageLiteralResourceName: pumpkinBottom[num])
             num  = num + 1
         }else {
             num = 0
-        }
+        }**/
+        
+        //First way to use use Int.random(in:0...3)
+       /**
+        pumpkinImageFront.image = UIImage(imageLiteralResourceName: pumpkinImage[(Int.random(in:0...3))]);
+        pumpkinImageBottom.image = UIImage(imageLiteralResourceName: pumpkinImage[(Int.random(in:0...3))]);
+        **/
+        //Using randomElement()
+        pumpkinImageFront.image = UIImage(imageLiteralResourceName: pumpkinImage.randomElement()!);
+        pumpkinImageBottom.image = UIImage(imageLiteralResourceName: pumpkinImage.randomElement()!)
+        
     }
     
 }
